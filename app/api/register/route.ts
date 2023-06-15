3aa7e08cf418b5263335797f3a5d64f039d6a7a0
rkshawon <rkshawon16@gmail.com>
2023-06-15 13:35:34 +0600

    protected client route

---
+++ app/api/register/route.ts
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+import prisma from "../../libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const { name, email, password } = body;
+
+    if (!name || !email || !password) {
+      return new NextResponse("Missing field", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log(error, "Registration Error");
+    return new NextResponse("Internal Error ", { status: 500 });
+  }
+}
